f63d3f76b37117fea380eafb8c93e405e7cfc696
Roghashin <rtimbiti@gmail.com>
2018-12-08 10:51:08 +0300

    add loader and some extra validation

--- UI/js/fetchscripts/login.js
+++ UI/js/fetchscripts/login.js
@@ -7,6 +7,48 @@ register = () => {
     password = document.getElementById("password").value;
     confirm = document.getElementById("confirm").value;
 
+    if (username.trim() === "") {
+        err_element = document.getElementById("notif-register")
+        err_element.classList.add("err-msg")
+        err_element.innerHTML = "Please provide a username"
+        return false
+    } else if (email.trim() === "") {
+        err_element = document.getElementById("notif-register")
+        err_element.classList.add("err-msg")
+        err_element.innerHTML = "Please provide an email"
+        return false
+    } else if (phone.trim() === "") {
+        err_element = document.getElementById("notif-register")
+        err_element.classList.add("err-msg")
+        err_element.innerHTML = "Please provide a phone number"
+        return false
+    } else if (address.trim() === "") {
+        err_element = document.getElementById("notif-register")
+        err_element.classList.add("err-msg")
+        err_element.innerHTML = "Please provide aa address"
+        return false
+    } else if (password.trim() === "") {
+        err_element = document.getElementById("notif-register")
+        err_element.classList.add("err-msg")
+        err_element.innerHTML = "Please provide a password"
+        return false
+    } else if (confirm.trim() === "") {
+        err_element = document.getElementById("notif-register")
+        err_element.classList.add("err-msg")
+        err_element.innerHTML = "Please confirm your password"
+        return false
+    }
+
+    var format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.match(format)) {
+
+    } else {
+        err_element = document.getElementById("notif-register")
+        err_element.classList.add("err-msg")
+        err_element.innerHTML = "Please provide a valid email"
+        return false
+    }
+
     if (password != confirm) {
         notif_register = document.getElementById("notif-register")
         if (notif_register.classList.contains("succ-msg")) {
@@ -29,6 +71,16 @@ register = () => {
         const url = "https://sendit-updated.herokuapp.com/api/v2/signup";
         // const url = "http://127.0.0.1:5000/api/v2/signup";
 
+        register_loader = document.getElementsByClassName("loader")[0]
+        register_btn = document.getElementsByClassName("btn")[0]
+        err_element = document.getElementById("notif-register")
+        if (err_element.classList.contains("err-msg")) {
+            err_element.classList.remove("err-msg")
+            err_element.style.display = "none"
+        }
+        register_loader.style.display = "block"
+        register_btn.style.display = "none"
+
         fetch(url, {
                 method: "POST",
                 body: JSON.stringify(data),
@@ -46,13 +98,20 @@ register = () => {
                     }
                     succ_element.classList.add("succ-msg")
                     succ_element.innerHTML = "You account has been successfully created"
+                    register_loader.style.display = "none"
+                    register_btn.style.display = "block"
+                    setTimeout(() => {
+                        location.reload()
+                    }, 2000)
                 } else {
                     err_element = document.getElementById("notif-register")
                     if (err_element.classList.contains("succ-msg")) {
-                        succ_element.classList.remove("succ-msg")
+                        err_element.classList.remove("succ-msg")
                     }
                     err_element.classList.add("err-msg")
                     err_element.innerHTML = message
+                    register_loader.style.display = "none"
+                    register_btn.style.display = "block"
                 }
             })
             .catch(err => console.log(err))
@@ -64,6 +123,20 @@ login = (user) => {
     event.preventDefault()
     username = document.getElementById("username_login").value;
     password = document.getElementById("password_login").value;
+
+    if (username.trim() === "") {
+        err_element = document.getElementById("notif-login")
+        err_element.classList.add("err-msg")
+        err_element.style.display = "block"
+        err_element.innerHTML = "Please provide a username"
+        return false
+    } else if (password.trim() === "") {
+        err_element = document.getElementById("notif-login")
+        err_element.classList.add("err-msg")
+        err_element.style.display = "block"
+        err_element.innerHTML = "Please provide a password"
+        return false
+    }
 
     const data = {
         username,
@@ -79,6 +152,16 @@ login = (user) => {
         // var url = "http://127.0.0.1:5000/api/v2/login"
         var home = "./order.html"
     }
+
+    err_element = document.getElementById("notif-login")
+    if(err_element.classList.contains("err-msg")){
+        err_element.classList.remove("err-msg")
+        err_element.style.display = "none"
+    }
+    login_loader = document.getElementsByClassName("loader")[1]
+    login_btn = document.getElementsByClassName("btn")[1]
+    login_loader.style.display = "block"
+    login_btn.style.display = "none"
 
     fetch(url, {
             method: "POST",
@@ -98,8 +181,11 @@ login = (user) => {
                 message = data["message"]
                 err_element = document.getElementById("notif-login")
                 err_element.classList.add("err-msg")
+                err_element.style.display = "block"
                 err_element.innerHTML = message
             }
+            login_loader.style.display = "none"
+            login_btn.style.display = "block"
         })
         .catch(err => console.log(err))
 
