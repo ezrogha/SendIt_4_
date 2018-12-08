ffe0ddc5cfa27300f0fb0b5fd23fe47892619022
Roghashin <rtimbiti@gmail.com>
2018-12-08 10:53:33 +0300

    Add redirection from admin login

--- UI/admin/js/fetchscripts/redirect.js
+++ UI/admin/js/fetchscripts/redirect.js
@@ -3,7 +3,11 @@ if (localStorage.getItem("token") != null) {
     data = JSON.parse(atob(token.split('.')[1]))
     userData = data["identity"]
     role = userData["role"]
+    console.log(userData)
     if (role == "admin") {
-        window.location.replace("./index.html");
+        window.location.href = "./index.html";
+    } else {
+        window.location.href = "./login.html";
     }
+
 }
